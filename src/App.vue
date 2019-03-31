6697494848ed6cad6e7c9f77fdfae1f29adff4c0
vo.trong.van <FRAMGIA\vo.trong.van@hcm-102-0004.framgia.com>
2019-03-31 20:17:47 +0700

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <img alt="Vue logo" src="./assets/logo.png" class="logo">
+    <!-- <TodoList></TodoList> -->
+    <todo-list></todo-list>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TodoList from './components/TodoList.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    TodoList
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo1",
+          completed: false
+        }
+      ]
+    }
   }
 }
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*text-align: center;*/
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.logo {
+  display: block;
+  height: 75px;
+  margin: 20px auto;
 }
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="What needs to be done" v-model="newTodo">
+
+    Todo list goes here
+  </div>
+</template>
+
+<script type="text/javascript">
+  export default {
+    name: 'todo-list',
+    data() {
+      return {
+        newTodo: '',
+        todos: [
+          {
+            id: 1,
+            title: 'Todo1',
+            completed: false
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+/*  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }*/
+
+  .todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
+
+
+  }
+
+
+</style>
